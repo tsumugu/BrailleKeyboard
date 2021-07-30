1dbc331c915ebaed9ef1d7a3d9c9f13dcb1c84f3
tsumugu <tsumugu1515@gmail.com>
2021-07-30 11:33:48 +0900

    style: Arduinoのスケッチを整形

--- Arduino/brailleKeyboardArduino/brailleKeyboardArduino.ino
+++ Arduino/brailleKeyboardArduino/brailleKeyboardArduino.ino
@@ -27,90 +27,62 @@ void setup() {
 void onReceiveWire(int bitstream) {
   //int ci = Wire.read();
   char c = Wire.read();
-  // 動かん...。
   switch (c) {
     case 0x00:  
-      Serial.write("0x0");
-      //
       digitalWrite(bootStatusLED, HIGH);
-      //
       digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x01:
-      Serial.write("0x01");
-      //
       digitalWrite(dakutenModeStatusLED, HIGH);
-      //
       //digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x02:
-      Serial.write("0x02");
-      //
       digitalWrite(hanDakutenModeStatusLED, HIGH);
-      //
       digitalWrite(dakutenModeStatusLED, LOW);
       //digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x03:
-      Serial.write("0x03");
-      //
       digitalWrite(suujiModeStatusLED, HIGH);
-      //
       digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       //digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x04:
-      Serial.write("0x04");
-      //
       digitalWrite(alphabetModeStatusLED, HIGH);
-      //
       digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       //digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x05:
-      Serial.write("0x05");
-      //
       digitalWrite(youonnModeStatusLED, HIGH);
-      //
       digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       //digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     case 0x06:
-      Serial.write("0x06");
-      //
       //digitalWrite(bootStatusLED, LOW);
       digitalWrite(dakutenModeStatusLED, LOW);
       digitalWrite(hanDakutenModeStatusLED, LOW);
       digitalWrite(suujiModeStatusLED, LOW);
       digitalWrite(alphabetModeStatusLED, LOW);
       digitalWrite(youonnModeStatusLED, LOW);
-      //
       break;
     default:
       Serial.write(c);
@@ -121,20 +93,4 @@ void onReceiveWire(int bitstream) {
 }
 
 void loop() {
-  /*
-  digitalWrite(bootStatusLED, HIGH);
-  digitalWrite(dakutenModeStatusLED, HIGH);
-  digitalWrite(hanDakutenModeStatusLED, HIGH);
-  digitalWrite(suujiModeStatusLED, HIGH);
-  digitalWrite(alphabetModeStatusLED, HIGH);
-  digitalWrite(youonnModeStatusLED, HIGH);
-  delay(1000);
-  digitalWrite(bootStatusLED, LOW);
-  digitalWrite(dakutenModeStatusLED, LOW);
-  digitalWrite(hanDakutenModeStatusLED, LOW);
-  digitalWrite(suujiModeStatusLED, LOW);
-  digitalWrite(alphabetModeStatusLED, LOW);
-  digitalWrite(youonnModeStatusLED, LOW);
-  delay(1000);
-  */
 }
